049b851f31f89af5b8e10c5ccbfaa1d6dde1f6ab
Mark Strefford <markstrefford@me.com>
2015-06-06 11:52:43 +0100

    Modified csv creator for prefix and exlude options
    Helps creating a list of files for training the vision algorithm

--- utils/create_csv.py
+++ utils/create_csv.py
@@ -2,13 +2,13 @@
 __author__ = 'markstrefford'
 __org__ = 'Timelaps Robotics'
 
-import sys
+import argparse, sys
 import os.path
 
 # This is a tiny script to help you creating a CSV file from a face
 # database with a similar hierarchie:
 #
-#  philipp@mango:~/facerec/data/at$ tree
+# philipp@mango:~/facerec/data/at$ tree
 #  .
 #  |-- README
 #  |-- s1
@@ -29,13 +29,27 @@ import os.path
 
 if __name__ == "__main__":
 
-    if len(sys.argv) != 3:
-        print "usage: create_csv <base_path> <csv_path>"
-        sys.exit(1)
+    #Parse arguments
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-i", "--images", required=True,
+                    help="path to the directory containing the images")
+    ap.add_argument("-c", "--csv", required=True,
+                    help="path to use for creating the csv file")
+    ap.add_argument("-p", "--prefix", required=False,
+                    help="Optional prefix for images to be indexed")
+    ap.add_argument("-e", "--exclude", required=False,
+                    help="Optional prefix for images to be excluded")
+    args = vars(ap.parse_args())
 
-    BASE_PATH=sys.argv[1]
-    CSV_PATH=sys.argv[2]
-    SEPARATOR=";"
+    # BASE_PATH=sys.argv[1]
+    # CSV_PATH=sys.argv[2]
+    BASE_PATH = args["images"]
+    CSV_PATH = args["csv"]
+    PREFIX = str(args["prefix"])
+    EXCLUDE = str(args["exclude"])
+    SEPARATOR = ";"
+
+    print "Creating csv for files with prefix " + PREFIX
 
     # TODO: Label should be the last directory name (so ideally the name of the person!)
     label = "n/a"
@@ -45,12 +59,15 @@ if __name__ == "__main__":
         for subdirname in dirnames:
             subject_path = os.path.join(dirname, subdirname)
             for filename in os.listdir(subject_path):
-            # Check we haven't processed this file already!
-                if filename.find("face") != 0:
-                    label = os.path.basename(subject_path)
-                    abs_path = "%s/%s" % (subject_path, filename)
-                    print "%s%s%s" % (abs_path, SEPARATOR, label)
-                    csvfile.write(abs_path + SEPARATOR + label + "\n")
+                # Check we haven't processed this file already!
+
+                # If we have a prefix check that
+                if EXCLUDE != "" and filename.find(EXCLUDE) != 0:
+                    if (PREFIX != "None" and filename.find(PREFIX) == 0 ) or PREFIX == "None":
+                        label = os.path.basename(subject_path)
+                        abs_path = "%s/%s" % (subject_path, filename)
+                        #print "%s%s%s" % (abs_path, SEPARATOR, label)
+                        csvfile.write(abs_path + SEPARATOR + label + "\n")
 
     csvfile.close()
 
